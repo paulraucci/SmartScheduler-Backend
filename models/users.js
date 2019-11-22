7118fc9f4f75a122dd6852e3fb1e7410e19e689a
Jenwng <Jenniescwong@gmail.com>
2019-11-22 12:10:07 -0500

    appointment routes curl tests passed.

--- models/users.js
+++ models/users.js
@@ -1,17 +1,18 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const { appointmentSchema } = require("./appointments.js")
+// const { appointmentSchema } = require("./appointments.js")
 
 const userSchema = Schema({
-    firstName: { type: String, require: true},
-    lastName: { type: String, require: true},
-    dob: { type: String, require: true},
-    email: { type: String, require: true},
-    phone: { type: String, require: true},
-    category: { type: String, default: 'patient'},
-    username: { type: String, require: true },
-    password: { type: String, require: true },
-    appointments: [appointmentSchema]
+  firstName: { type: String, require: true },
+  lastName: { type: String, require: true },
+  dob: { type: String, require: true },
+  email: { type: String, require: true },
+  phone: { type: String, require: true },
+  category: { type: String, default: "patient" },
+  username: { type: String, require: true },
+  password: { type: String, require: true },
+  appointments: []
+  //   appointments: [appointmentSchema]
 });
 
 const User = mongoose.model("User", userSchema);
