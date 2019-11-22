89c080b5bb3bb91d7e12781057bdae11493c3f2f
Jenwng <Jenniescwong@gmail.com>
2019-11-21 23:05:50 -0500

    curl test user routes, fixed and passed

--- controllers/user.js
+++ controllers/user.js
@@ -1,45 +1,51 @@
 const express = require("express");
 const user = express.Router();
-const User = require('../models/users.js')
+const User = require("../models/users.js");
 
 //Index Route
-user.get('/', (req,res) => {
-    User.find({}, (err, foundUser) => {
-        if (err) {
-            res.status(400).json({ err: err.message })
-        }
-        res.status(200).json(foundUser)
-    })
-})
+user.get("/", (req, res) => {
+  User.find({}, (err, foundUser) => {
+    if (err) {
+      res.status(400).json({ err: err.message });
+    }
+    res.status(200).json(foundUser);
+  });
+});
 
 //Create Route
-user.post('/', (req,res) => {
-    User.create(req.body, (err, createdUser) => {
-        if (err) {
-            res.status(400).json({ err: err.message })
-        }
-        res.status(200).send(createdUser)
-    })
-})
-
-//Delete Route
-user.delete('/:id', (req,res) => {
-    User.findByIdAndRemove(req.params.id, (error, deletedUser) => {
-        if (error) {
-            res.status(400).json({ error: error,message })
-        }
-        res.status(200).json(deletedUser)
-    })
-})
+user.post("/", (req, res) => {
+  User.create(req.body, (err, createdUser) => {
+    if (err) {
+      res.status(400).json({ err: err.message });
+    } else {
+      res.status(200).send(createdUser);
+    }
+  });
+});
 
 //Update Route
-user.put('/:id', (req,res) => {
-    User.findByIdAndUpdate(req.params.id, req.body, { new: true }, (error, updatedUser) => {
-        if (err) {
-            res.status(400).json({ err: err.message})
-        }
-        res.status(200).json(updatedUser)
-    })
-})
+user.put("/:id", (req, res) => {
+  User.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    { new: true },
+    (err, updatedUser) => {
+      if (err) {
+        res.status(400).json({ err: err.message });
+      }
+      res.status(200).json(updatedUser);
+    }
+  );
+});
 
-module.exports = user
+//Delete Route
+user.delete("/:id", (req, res) => {
+  User.findByIdAndRemove(req.params.id, (error, deletedUser) => {
+    if (error) {
+      res.status(400).json({ error: error, message });
+    }
+    res.status(200).json(deletedUser);
+  });
+});
+
+module.exports = user;
